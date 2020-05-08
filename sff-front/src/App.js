bac2090ab190ed85f32fc024911d872f95a10f88
aleksandertsg <aleksander.tsoganov@gmail.com>
2020-05-08 19:09:59 +0300

    Simple connect web3portal + portis

--- sff-front/src/App.js
+++ sff-front/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import web3Modal from './util/web3Modal'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  componentDidMount = async () => {
+    const web3 = await web3Modal.loadProvider()
+    const accounts = await web3.eth.getAccounts()
+    console.log({ accounts })
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ sff-front/src/util/web3Modal.js
@@ -0,0 +1,30 @@
+import Web3 from 'web3'
+import Web3Modal from 'web3modal'
+import Portis from '@portis/web3'
+
+
+console.log(process.env.REACT_APP_PORTIS_ID)
+
+const loadProvider = async () => {
+  const providerOptions = {
+    portis: {
+      package: Portis,
+      options: {
+        id: process.env.REACT_APP_PORTIS_ID || 'PORTIS_ID'
+      }
+    }
+  }
+
+  const web3Modal = new Web3Modal({
+    network: 'mainnet', // optional
+    cacheProvider: false, // optional
+    providerOptions // required
+  })
+
+  const provider = await web3Modal.connect()
+  const web3 = new Web3(provider)
+
+  return web3
+}
+
+export default { loadProvider }
